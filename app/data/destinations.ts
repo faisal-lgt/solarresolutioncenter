74034024a9d2b4f8b99ec959ca5155ecc85e9a29
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2026-02-21 15:13:22 +0500

    Initial Commit

--- app/data/destinations.ts
+++ app/data/destinations.ts
@@ -9,18 +9,18 @@ export const list: TDestination[] = [
             pixelId: "1359467079289793",
         }
     },
-    {
-        name: "stonegate",
-        url: "https://quiz.stonegateclaimcenter.com/stonegate-solar-relief",
-        params: {
-            oid: "18",
-            affid: "39",
-
-            c: "6650",
-            pixelId: "1359467079289793",
-        },
-        hours: [7, 18]
-    },
+    // {
+    //     name: "stonegate",
+    //     url: "https://quiz.stonegateclaimcenter.com/stonegate-solar-relief",
+    //     params: {
+    //         oid: "18",
+    //         affid: "39",
+    //
+    //         c: "6650",
+    //         pixelId: "1359467079289793",
+    //     },
+    //     hours: [7, 18]
+    // },
 ];
 
 export default list;
